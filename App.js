4693ad9ccbeabbd8b34a851043d14203f870798e
Michael Wood <esiotrot@gmail.com>
2024-03-25 13:21:36 +0200

    Reformat App.js using prettier

--- App.js
+++ App.js
@@ -1,81 +1,83 @@
 import { NavigationContainer } from '@react-navigation/native';
 import React from 'react';
-import { createNativeStackNavigator } from "@react-navigation/native-stack";
-import { Login, ClientSignup, TrainerSignup, Welcome, Who, HomeScreen, Profiles, MapView} from "./screens";
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import {
+  Login,
+  ClientSignup,
+  TrainerSignup,
+  Welcome,
+  Who,
+  HomeScreen,
+  Profiles,
+  MapView,
+} from './screens';
 import Ionicons from 'react-native-vector-icons/Ionicons';
 import MyTabs from './navigation/MyTabs';
+
 const Stack = createNativeStackNavigator();
 
 export default function App() {
-  
   return (
-      
     <NavigationContainer>
-      <Stack.Navigator
-        initialRouteName='Welcome'
-      >
+      <Stack.Navigator initialRouteName="Welcome">
         <Stack.Screen
           name="Welcome"
           component={Welcome}
           options={{
-            headerShown: false
+            headerShown: false,
           }}
         />
         <Stack.Screen
           name="Who"
           component={Who}
-          options= {{
-            headerShown: false
+          options={{
+            headerShown: false,
           }}
-          />
+        />
         <Stack.Screen
           name="Login"
           component={Login}
           options={{
-            headerShown: false
+            headerShown: false,
           }}
         />
         <Stack.Screen
           name="ClientSignup"
           component={ClientSignup}
           options={{
-            headerShown: false
+            headerShown: false,
           }}
         />
         <Stack.Screen
-        name="TrainerSignup"
-        component={TrainerSignup}
-        options={{
-        headerShown: false
-        }}
+          name="TrainerSignup"
+          component={TrainerSignup}
+          options={{
+            headerShown: false,
+          }}
         />
         <Stack.Screen
-        name="HomeScreen"
-        component={HomeScreen}
-        options={{
-          headerShown: false
-        }}
+          name="HomeScreen"
+          component={HomeScreen}
+          options={{
+            headerShown: false,
+          }}
         />
         <Stack.Screen
-        name="Profiles"
-        component={Profiles}
-        options={{
-          headerShown: false
-        }}
+          name="Profiles"
+          component={Profiles}
+          options={{
+            headerShown: false,
+          }}
         />
         <Stack.Screen
-        name="MapView"
-        component={MapView}
-        options={{
-          headerShown: false
-        }}
+          name="MapView"
+          component={MapView}
+          options={{
+            headerShown: false,
+          }}
         />
-        <Stack.Screen
-        name= 'My Tabs'
-        component={MyTabs}
-        />
-        </Stack.Navigator>
-        
+        <Stack.Screen name="My Tabs" component={MyTabs} />
+      </Stack.Navigator>
     </NavigationContainer>
   );
 }
